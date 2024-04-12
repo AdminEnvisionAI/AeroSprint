f56703b2b41063776721ebd7f814f103a5274921
Admin EnvisionAI <envisionaicontactus@gmail.com>
2024-04-12 03:53:11 -0400

    UI refinement and bug fixes

--- src/Requirement/RequirementGeneration.js
+++ src/Requirement/RequirementGeneration.js
@@ -178,7 +178,7 @@ export default function HorizontalNonLinearStepper() {
         )}
       </div>
     </Box>
-    <Footer />
+    {/* <Footer /> */}
     </DashboardLayout>
   );
 }
--- src/Requirement/Settings.js
+++ src/Requirement/Settings.js
@@ -13,6 +13,9 @@ import "tippy.js/dist/tippy.css";
 import MuiCheckbox from "@mui/material/Checkbox";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import Text from "@mui/material/TextField"
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
 
 const Requirement = ({ startLoading, stopLoading }) => {
@@ -25,17 +28,17 @@ const Requirement = ({ startLoading, stopLoading }) => {
     industry: "",
     domain: "",
     subdomain: "",
-    corearea: "", 
+    corearea: "",
     compliance: ""
   });
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });   
+    setFormData({ ...formData, [name]: value });
   };
   dispatch(settingData(formData));
   return (
-    <div>
+    // <div>
       <div className="container">
         <div className="row gx-0">
           <div className="col-12 story-column">
@@ -60,24 +63,25 @@ const Requirement = ({ startLoading, stopLoading }) => {
             <div className="story-column-4">
               <div className="setting_session_flex">
                 <div className="keywords_box">
-                <h5
-                  className="import_story_text"
-                  style={{ marginTop: "0.5rem", width: "40.5%" }}
-                >
-                  Industry
-                </h5>
-                <select
-                  name="industry"
-                  // value={formData.compliance}
-                  onChange={handleChange}
-                  id="cars"
-                  style={{ width: "29.4%" }}
-                  className="compilance_select"
-                >
-                  <option>Select</option>
-                  <option value="BFSI">BFSI</option>
-                </select>
-              </div>
+                  <h5
+                    className="import_story_text"
+                    style={{ marginTop: "0.5rem", width: "37%" }}
+                  >
+                    Industry
+                  </h5>
+                  <Select
+                    name="industry"
+                    value={formData.industry}
+                    onChange={handleChange}
+                    id="cars"
+                    style={{ width: "29.4%" }}
+                    className="muiselect"
+                    // className="compilance_select"
+                  >
+                    <MenuItem value={0}>Select</MenuItem>
+                    <MenuItem value={"BFSI"}>BFSI</MenuItem>
+                  </Select>
+                </div>
                 <div className="keywords_box">
                   <h5
                     className="import_story_text"
@@ -85,59 +89,60 @@ const Requirement = ({ startLoading, stopLoading }) => {
                   >
                     Domain
                   </h5>
-                  <select
+                  <Select
                     name="domain"
                     // value={setting.industry}
                     onChange={handleChange}
                     id="cars"
+                    className="muiselect"
                     style={{ width: "65%", marginLeft: "4%" }}
                   >
-                    <option>Select</option>
-                    <option value="Banking">Banking</option>
-                    <option value="Insurance">Insurance</option>
-                    <option value="CapitalMarkets">Capital Markets</option>
-                    <option value="OtherFinancialServices">Other Financial Services</option>                    
-                  </select>
+                    <MenuItem value="Select">Select</MenuItem>
+                    <MenuItem value="Banking">Banking</MenuItem>
+                    <MenuItem value="Insurance">Insurance</MenuItem>
+                    <MenuItem value="CapitalMarkets">Capital Markets</MenuItem>
+                    <MenuItem value="OtherFinancialServices">Other Financial Services</MenuItem>
+                  </Select>
                 </div>
-                </div>
-                <div className="setting_session_flex">
+              </div>
+              <div className="setting_session_flex">
                 <div className="keywords_box">
                   <h5
                     className="import_story_text"
-                    style={{ marginTop: "0.5rem", width: "46%" }}
+                    style={{ marginTop: "0.5rem", width: "40.5%" }}
                   >
                     Sub Domain
                   </h5>
-                  <select
-                    name="domain"                  
+                  <Select
+                    name="domain"
                     onChange={handleChange}
                     id="cars"
-                    style={{ width: "65%", marginLeft: "0.3rem" }}
+                    className="muiselect"
                   >
-                    <option>Select</option>
-                    <option value="Retail Banking">Retail Banking</option>
-                    <option value="Commercial Banking">Commercial Banking</option>
-                    <option value="Investment Banking">Investment Banking</option>
-                    <option value="Private Banking">Private Banking</option>
-                    <option value="Asset Management">Asset Management</option>
-                    <option value="Wealth Management">Wealth Management</option>
-                    <option value="Corporate Banking">Corporate Banking</option>
-                    <option value="Treasury Services">Treasury Services</option>
-                    <option value="Risk Management">Risk Management</option>
-                    <option value="Technology and Innovation">Technology and Innovation</option>
-                    <option value="Regulatory Compliance">Regulatory Compliance</option>
-                    <option value="Credit and Lending">Credit and Lending</option>
-                    <option value="Mortgage Services">Mortgage Services</option>
-                    <option value="Payment Processing">Payment Processing</option>
-                    <option value="Customer Service and Support">Customer Service and Support</option>
-                    <option value="Financial Planning and Advisory">Financial Planning and Advisory</option>
-                    <option value="Capital Markets">Capital Markets</option>
-                    <option value="Insurance Services (for banks offering insurance products)">Insurance Services (for banks offering insurance products)</option>
-                    <option value="Foreign Exchange Services">Foreign Exchange Services</option>
-                    <option value="Trade Finance">Trade Finance</option>                    
-                  </select>
+                    <MenuItem value="Select">Select</MenuItem>
+                    <MenuItem value="Retail Banking">Retail Banking</MenuItem>
+                    <MenuItem value="Commercial Banking">Commercial Banking</MenuItem>
+                    <MenuItem value="Investment Banking">Investment Banking</MenuItem>
+                    <MenuItem value="Private Banking">Private Banking</MenuItem>
+                    <MenuItem value="Asset Management">Asset Management</MenuItem>
+                    <MenuItem value="Wealth Management">Wealth Management</MenuItem>
+                    <MenuItem value="Corporate Banking">Corporate Banking</MenuItem>
+                    <MenuItem value="Treasury Services">Treasury Services</MenuItem>
+                    <MenuItem value="Risk Management">Risk Management</MenuItem>
+                    <MenuItem value="Technology and Innovation">Technology and Innovation</MenuItem>
+                    <MenuItem value="Regulatory Compliance">Regulatory Compliance</MenuItem>
+                    <MenuItem value="Credit and Lending">Credit and Lending</MenuItem>
+                    <MenuItem value="Mortgage Services">Mortgage Services</MenuItem>
+                    <MenuItem value="Payment Processing">Payment Processing</MenuItem>
+                    <MenuItem value="Customer Service and Support">Customer Service and Support</MenuItem>
+                    <MenuItem value="Financial Planning and Advisory">Financial Planning and Advisory</MenuItem>
+                    <MenuItem value="Capital Markets">Capital Markets</MenuItem>
+                    <MenuItem value="Insurance Services (for banks offering insurance products)">Insurance Services (for banks offering insurance products)</MenuItem>
+                    <MenuItem value="Foreign Exchange Services">Foreign Exchange Services</MenuItem>
+                    <MenuItem value="Trade Finance">Trade Finance</MenuItem>
+                  </Select>
                 </div>
-               
+
                 <div className="keywords_box">
                   <h5
                     className="import_story_text"
@@ -145,56 +150,69 @@ const Requirement = ({ startLoading, stopLoading }) => {
                   >
                     Core Area
                   </h5>
-                  <select
+                  <Select
+                  type="select"
                     name="corearea"
                     value={setting.corearea}
                     onChange={handleChange}
                     id="cars"
+                    className="muiselect"
                     style={{ width: "65%", marginLeft: "0.3rem" }}
                   >
-                    <option>Select</option>
-                    <option value="CapitalMarkets">Capital Markets</option>
-                    <option value="DepositAccounts">Deposit Accounts</option>
-                    <option value="ConsumerLoans">Consumer Loans</option>
-                    <option value="CreditCards">Credit Cards</option>
-                    <option value="Mortgages">Mortgages</option>
-                    <option value="DebitCards">Debit Cards</option>
-                    <option value="OnlineandMobileBanking">Online and Mobile Banking</option>
-                    <option value="ATMServices">ATM Services</option>
-                    <option value="BranchBanking">Branch Banking</option>
-                    <option value="CustomerServiceandSupport">Customer Service and Support</option>
-                    <option value="FinancialEducationandCounseling">Financial Education and Counseling</option>
-                    <option value="OverdraftProtection">Overdraft Protection</option>
-                    <option value="FraudPreventionandSecurity">Fraud Prevention and Security</option>
-                    <option value="RewardsandLoyaltyPrograms">Rewards and Loyalty Programs</option>
-                    <option value="ForeignCurrencyServices">Foreign Currency Services</option>
-                    <option value="SmallBusinessBanking">Small Business Banking</option>
-                  </select>
+                    <MenuItem value="Select">Select</MenuItem>
+                    <MenuItem value="CapitalMarkets">Capital Markets</MenuItem>
+                    <MenuItem value="DepositAccounts">Deposit Accounts</MenuItem>
+                    <MenuItem value="ConsumerLoans">Consumer Loans</MenuItem>
+                    <MenuItem value="CreditCards">Credit Cards</MenuItem>
+                    <MenuItem value="Mortgages">Mortgages</MenuItem>
+                    <MenuItem value="DebitCards">Debit Cards</MenuItem>
+                    <MenuItem value="OnlineandMobileBanking">Online and Mobile Banking</MenuItem>
+                    <MenuItem value="ATMServices">ATM Services</MenuItem>
+                    <MenuItem value="BranchBanking">Branch Banking</MenuItem>
+                    <MenuItem value="CustomerServiceandSupport">Customer Service and Support</MenuItem>
+                    <MenuItem value="FinancialEducationandCounseling">Financial Education and Counseling</MenuItem>
+                    <MenuItem value="OverdraftProtection">Overdraft Protection</MenuItem>
+                    <MenuItem value="FraudPreventionandSecurity">Fraud Prevention and Security</MenuItem>
+                    <MenuItem value="RewardsandLoyaltyPrograms">Rewards and Loyalty Programs</MenuItem>
+                    <MenuItem value="ForeignCurrencyServices">Foreign Currency Services</MenuItem>
+                    <MenuItem value="SmallBusinessBanking">Small Business Banking</MenuItem>
+                  </Select>
                 </div>
-                </div>
-                <div className="setting_session_flex">
+              </div>
+              <div className="setting_session_flex">
                 <div className="keywords_box">
                   <h5
                     className="import_story_text"
-                    style={{ marginTop: "0.5rem", width: "18%" }}
+                    style={{ marginTop: "0.5rem", width: "17%" }}
                   >
                     Applicable Compliance
                   </h5>
                   <FormGroup row={true}>
                     <FormControlLabel control={<MuiCheckbox defaultChecked />} label="ISO 27001" />
-                    <FormControlLabel  control={<MuiCheckbox />} label="GDPR" />
-                    <FormControlLabel  control={<MuiCheckbox />} label="WCAG 2.0 AA" />
-                    <FormControlLabel  control={<MuiCheckbox />} label="PSD2" />
-                    <FormControlLabel  control={<MuiCheckbox />} label="ADA" />
-                    <FormControlLabel  control={<MuiCheckbox />} label="HIPAA" />
+                    <FormControlLabel control={<MuiCheckbox />} label="GDPR" />
+                    <FormControlLabel control={<MuiCheckbox />} label="WCAG 2.0 AA" />
+                    <FormControlLabel control={<MuiCheckbox />} label="PSD2" />
+                    <FormControlLabel control={<MuiCheckbox />} label="ADA" />
+                    <FormControlLabel control={<MuiCheckbox />} label="HIPAA" />
                   </FormGroup>
+                </div>
+              </div>
+              <div className="setting_session_flex">
+                <div className="keywords_box">
+                  <h5
+                    className="import_story_text"
+                    style={{ marginTop: "0.5rem", width: "17%" }}
+                  >
+                    Keywords
+                  </h5>
+                 <Text type="input" style={{ width: "40%", marginLeft: "0.0rem" }}/>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    // </div>
   );
 };
 
