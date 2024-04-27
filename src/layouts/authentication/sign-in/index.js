24257afd21e9a0ad04524626e0250af57bd91622
ManishWE <Manish@wannaeat.com>
2024-04-27 19:52:03 +0530

    Dummy Login

--- src/layouts/authentication/sign-in/index.js
+++ src/layouts/authentication/sign-in/index.js
@@ -1,18 +1,3 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import { useState } from "react";
 
 // react-router-dom components
@@ -40,6 +25,7 @@ import BasicLayout from "layouts/authentication/components/BasicLayout";
 
 // Images
 import bgImage from "assets/images/bg-sign-in-basic.jpeg";
+import Dashboard from "layouts/dashboard";
 
 function Basic() {
   const [rememberMe, setRememberMe] = useState(false);
@@ -102,8 +88,19 @@ function Basic() {
               </MDTypography>
             </MDBox>
             <MDBox mt={4} mb={1}>
-              <MDButton variant="gradient" color="info" fullWidth>
-                sign in
+              <MDButton variant="gradient" color="info" fullWidth>                
+                <MDTypography variant="button" color="info">
+                <MDTypography
+                  component={Link}
+                  to="/dashboard"
+                  variant="button"
+                  color="white"
+                  fontWeight="medium"
+                >
+                  Sign In
+                </MDTypography>
+              </MDTypography>
+              
               </MDButton>
             </MDBox>
             <MDBox mt={3} mb={1} textAlign="center">
